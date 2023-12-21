6e35dc8dc3efa3ff84e7ba761fa45082bc631e5f
Shing Chan <chan.shing@protonmail.com>
2023-12-21 08:14:19 +0000

    feat: Add recursively imports for modules

---
+++ modules/default.nix
@@ -0,0 +1,35 @@
+# Recursively imports all the modules
+# Modified from https://github.com/evanjs/nixos_cfg/blob/4bb5b0b84a221b25cf50853c12b9f66f0cad3ea4/config/new-modules/default.nix
+
+{ lib, ... }:
+
+with lib;
+let
+  # Recursively constructs an attrset of a given folder, recursing on directories, value of attrs is the filetype
+  getDir = dir: mapAttrs
+    (file: type:
+      if type == "directory" then getDir "${dir}/${file}" else type
+    )
+    (builtins.readDir dir);
+
+  # Collects all files of a directory as a list of strings of paths
+  files = dir: collect isString (mapAttrsRecursive (path: type: concatStringsSep "/" path) (getDir dir));
+
+  # Filters out directories that don't end with .nix or are this file, also makes the strings absolute
+  validFiles = dir: map
+    (file: ./. + "/${file}")
+    (filter
+      (file: hasSuffix ".nix" file && file != "default.nix" 
+      && ! (elem file config.modules.excludes))
+      (files dir));
+
+in
+{
+  options.modules.excludes = mkOption {
+    type = types.list types.str;
+    description = "A list of paths that should not be imported";
+  }
+
+  imports = validFiles ./.;
+
+}

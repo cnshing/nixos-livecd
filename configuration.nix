cdb3e4b4b4b4b235c9428752d86de5d870bba8b2
Shing Chan <chan.shing@protonmail.com>
2023-12-22 05:57:28 +0000

    fix: Remove dangerous system.stateVersion assignment in configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -19,8 +19,6 @@
 
   networking.firewall.allowedTCPPorts = [80];
 
-  system.stateVersion = lib.version;
-
   users.users.root.password = "nixos";
   services.openssh.settings.PermitRootLogin = lib.mkForce "yes";
   services.getty.autologinUser = lib.mkDefault "root";

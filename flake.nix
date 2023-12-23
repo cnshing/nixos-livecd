9d9a6c9af1fbf9dd2217d25ed3ee480e14149ef1
Shing Chan <chan.shing@protonmail.com>
2023-12-23 08:15:43 +0000

    build: Set home-manager flake dependecy version to 23.11

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
       url = "github:NixOS/nixpkgs/nixos-23.11";
     };
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-23.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };

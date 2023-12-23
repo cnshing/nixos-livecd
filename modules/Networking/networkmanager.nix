90dc609cb694599bb4c91dc828794d398e0f748e
Shing Chan <chan.shing@protonmail.com>
2023-12-23 07:11:39 +0000

    style: Format networking configuration with nesting in networkmanager.nix

--- modules/Networking/networkmanager.nix
+++ modules/Networking/networkmanager.nix
@@ -1,11 +1,13 @@
 # Everything required to get network manager working
 { 
-  networking.networkmanager.enable = true;
-  # Workaround for network manager and wpa_supp conflicts
-  # See https://github.com/NixOS/nixpkgs/blob/master/nixos/doc/manual/configuration/network-manager.section.md
-  networking.networkmanager.unmanaged = [
-    "*" "except:type:wwan" "except:type:gsm"
-  ];
-  networking.useDHCP = lib.mkForce true;
-  networking.firewall.allowedTCPPorts = [80];
+  networking = {
+    networkmanager.enable = true;
+    # Workaround for network manager and wpa_supp conflicts
+    # See https://github.com/NixOS/nixpkgs/blob/master/nixos/doc/manual/configuration/network-manager.section.md
+    networkmanager.unmanaged = [
+      "*" "except:type:wwan" "except:type:gsm"
+    ];
+    useDHCP = lib.mkForce true;
+    firewall.allowedTCPPorts = [80];
+  };
 }

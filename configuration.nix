169dff61bea7b9a27378b240ec48e4b35e1bd4df
Shing Chan <chan.shing@protonmail.com>
2023-12-23 04:39:34 +0000

    fix: Prevent network manager and wpa_supplicant conflicts

--- configuration.nix
+++ configuration.nix
@@ -18,6 +18,12 @@
   services.sshd.enable = true;
   services.nginx.enable = true;
 
+
+  # Workaround for network manager and wpa_supp conflicts
+  # See https://github.com/NixOS/nixpkgs/blob/master/nixos/doc/manual/configuration/network-manager.section.md
+  networking.networkmanager.unmanaged = [
+    "*" "except:type:wwan" "except:type:gsm"
+  ];
   networking.useDHCP = lib.mkDefault true;
   networking.firewall.allowedTCPPorts = [80];
 

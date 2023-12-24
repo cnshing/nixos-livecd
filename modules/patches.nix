aa4c0027e72e1664ed1af170696d5f6aff2b54db
Shing Chan <chan.shing@protonmail.com>
2023-12-24 06:38:32 +0000

    fix: Prevent Xlibs from being disabled with docker build

---
+++ modules/patches.nix
@@ -0,0 +1,7 @@
+# Technical hotfixes not belonging to any specific livecd feature/requirement 
+{ lib, ... }:
+{
+    # Fixes missing cairo-lib dependency for docker builds only
+    # See https://github.com/NixOS/nixpkgs/issues/119841
+    environment.noXlibs = lib.mkIf boot.isContainer lib.mkForce false;
+}

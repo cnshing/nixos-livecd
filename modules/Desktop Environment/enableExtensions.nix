cc7a557d6ce910f347c078f8c29691f4215297c8
Shing Chan <chan.shing@protonmail.com>
2023-12-31 02:56:56 +0000

    fix: Correct encapsulation syntaxes in enableExtensions.nix

--- modules/Desktop Environment/enableExtensions.nix
+++ modules/Desktop Environment/enableExtensions.nix
@@ -1,8 +1,10 @@
 {pkgs, ...}: {
   programs.dconf.enable = true;
-  profiles.${config.user.name}.databases = [
+  # Example modified from
+  # https://github.com/Electrostasy/dots/blob/c62895040a8474bba8c4d48828665cfc1791c711/profiles/system/gnome/default.nix#L235-L241
+  profiles.user.databases = [
     {
-      settings = {
+      settings = with lib.gvariant; {
         "org/gnome/shell".enabled-extensions = builtins.map (gnome: gnome.extensionUuid) (with pkgs; [
           gnomeExtensions.arcmenu
           gnomeExtensions.dash-to-panel
@@ -11,7 +13,7 @@
           gnomeExtensions.gsconnect
           gnomeExtensions.noannoyance-fork
           gnomeExtensions.awesome-tiles
-        ]) 
+        ]);
       };
     }
   ];

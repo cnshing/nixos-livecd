dc07aeda39251ec1c7e0a57083fa465aa62e83f3
Shing Chan <chan.shing@protonmail.com>
2024-01-10 08:01:05 +0000

    revert: Remove dead code with enableGnomeExtensions in browsers.nix

--- modules/Browser/browsers.nix
+++ modules/Browser/browsers.nix
@@ -7,6 +7,5 @@
   programs.firefox.enable = true;
 
   programs.firefox.nativeMessagingHosts.packages = with pkgs; [gnome-browser-connector];
-  nixpkgs.config.firefox.enableGnomeExtensions = true;
   programs.chromium.enable = true;
 }

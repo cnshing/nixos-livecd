646beeaaf986529ea0cc6021b41408887af015f7
Shing Chan <chan.shing@protonmail.com>
2024-01-10 07:09:56 +0000

    refactor: Update deprecrated enableGnomeExtension config with nativeMessagingHosts

--- modules/Browser/browsers.nix
+++ modules/Browser/browsers.nix
@@ -5,6 +5,8 @@
     ungoogled-chromium
   ];
   programs.firefox.enable = true;
+
+  programs.firefox.nativeMessagingHosts.packages = with pkgs; [gnome-browser-connector];
   nixpkgs.config.firefox.enableGnomeExtensions = true;
   programs.chromium.enable = true;
 }

8e241595e7d105be8816535fb886d3a91387e15a
Shing Chan <chan.shing@protonmail.com>
2024-01-15 09:01:01 +0000

    fix: Attempt fix by manually let recursive import ignore any disabledModules module

--- modules/default.nix
+++ modules/default.nix
@@ -1,15 +1,16 @@
 # Recursively imports all the modules
 # Modified from https://github.com/evanjs/nixos_cfg/blob/4bb5b0b84a221b25cf50853c12b9f66f0cad3ea4/config/new-modules/default.nix
-
-{ lib, ... }:
-
-with lib;
-let
+{lib, ...}:
+with lib; let
   # Recursively constructs an attrset of a given folder, recursing on directories, value of attrs is the filetype
-  getDir = dir: mapAttrs
-    (file: type:
-    # Implement implicit copy fix from https://discourse.nixos.org/t/understanding-the-import-system-in-nixos/24367/4
-      if type == "directory" then getDir (dir + "/${file}") else type
+  getDir = dir:
+    mapAttrs
+    (
+      file: type:
+      # Implement implicit copy fix from https://discourse.nixos.org/t/understanding-the-import-system-in-nixos/24367/4
+        if type == "directory"
+        then getDir (dir + "/${file}")
+        else type
     )
     (builtins.readDir dir);
 
@@ -17,13 +18,12 @@ let
   files = dir: collect isString (mapAttrsRecursive (path: type: concatStringsSep "/" path) (getDir dir));
 
   # Filters out directories that don't end with .nix or are this file, also makes the strings absolute
-  validFiles = dir: map
+  validFiles = dir:
+    map
     (file: ./. + "/${file}")
     (filter
-      (file: hasSuffix ".nix" file && file != "default.nix")
+      (file: hasSuffix ".nix" file && file != "default.nix" && builtins.any (module: builtins.typeOf module == "string" && builtins.match file module) disabledModules)
       (files dir));
-
-in
-{
+in {
   imports = validFiles ./.;
 }
